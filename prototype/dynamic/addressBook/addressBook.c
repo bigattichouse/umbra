d3ef62fe9812243f72bf7a488b4a007fc8a13144
Michael Johnson <michael.johnson@limitless-adventures.com>
2021-09-26 16:28:05 -0500

    Playing with dynamic loading of the db pages

---
+++ prototype/dynamic/addressBook/addressBook.c
@@ -0,0 +1,6 @@
+/*
+This is the shared library that counts and dynamically loads data pages,
+it provides the API used by apps to access information.
+
+Later this will include inserts and updates, for now just read only.
+*/
--- prototype/dynamic/umbra.c
+++ prototype/dynamic/umbra.c
@@ -3,22 +3,36 @@
 #include <time.h>
 
 #include "umbraMetadata.h"
-#include "addressBook.h" 
+#include "addressBook/addressBook.h"
 
+/*
+So, each "table" has a standard interface:  pageCount, loadPage
+
+Each of those modules has a recordCount(), record(position).
+Internally those modules will report back a record or NULL to prevent positions beyond the count.
+
+Later add an "insert" record.
+
+SO for this iteration of umbra, we're dynamically loading the addressBook table
+
+
+*/
 
 int main(int argc, char *argv[] ) {
    void *handle;
    void (*load_page)();
    Customers *data;
 
-   handle = dlopen("addressBook/bin/addressBook_0.so", RTLD_LAZY);
+   handle = dlopen("addressBook/bin/addressBook.so", RTLD_LAZY);
+
+
 
    if (!handle) {
         /* fail to load the library */
         fprintf(stderr, "Error: %s\n", dlerror());
         return EXIT_FAILURE;
    }
-   
+
    *(void**)(&load_page) = dlsym(handle, "load_page");
     if (!func_print_name) {
         /* no such symbol */
@@ -30,12 +44,12 @@ int main(int argc, char *argv[] ) {
     data = loadpage();
     Customer *rec;
     pos = 0;
-      
+
     while((rec = data[pos])!=NULL){
-      printf("%d.Name = %s\n",pos,rec.name);  
+      printf("%d.Name = %s\n",pos,rec.name);
       pos++;
     }
-        
+
     dlclose(handle);
 
     return EXIT_SUCCESS;
