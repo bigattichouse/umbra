bb36a8c39612a808733cd268102ced1f793108cd
Michael Johnson <michael.johnson@limitless-adventures.com>
2021-09-27 10:10:39 -0500

    refactoring/reorganization

--- prototype/dynamic/addressBook/build.sh
+++ prototype/dynamic/addressBook/build.sh
@@ -1,4 +1,4 @@
-gcc addressBook.c -o addressBook -ldl
+gcc pages/addressBookPages.c addressBook.c -o addressBook -ldl
 
 pushd pages/
 gcc -ldl -o ../bin/addressBookData.0.so addressBookData.0.c  -shared -fPIC
